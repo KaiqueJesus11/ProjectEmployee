fdacbf2b26688626cfc5e8e6eb1112d6055bfd4d
KaiqueJesus11 <kaique.jesus11@hotmail.com>
2019-12-26 04:05:17 -0300

    Layalt da tabela ajustada para Delete

--- APP/src/components/EmployeesList.js
+++ APP/src/components/EmployeesList.js
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet, FlatList, TouchableOpacity, Dimensions} from 're
 
 
 import api from '../services/api'
+import { red } from '@material-ui/core/colors'
 const width = Dimensions.get('window').width
 
 export default function EmployeesList(Name){
@@ -15,14 +16,18 @@ export default function EmployeesList(Name){
         loadEmployees()
     },[])   
 
-
+    async function deleteEmployee() {
+        await api.delete('/Employees?EmployeeID=',1 )
+        alert('Sucess!');    
+    }
     return(
         <View style={styles.container}>
         <View style = {styles.head}>
-        <View style = {styles.headCel}><Text style = {styles.listText}>Name</Text></View>
-        <View style = {styles.headCel}><Text style = {styles.listText}>Position</Text></View>
-        <View style = {styles.headCel}><Text style = {styles.listText}>Salary</Text></View>
-        <View style = {styles.headCel}><Text style = {styles.listText}>Age</Text></View>
+        <View style = {styles.headCel}><Text>Name</Text></View>
+        <View style = {styles.headCelPosition}><Text>Position</Text></View>
+        <View style = {styles.headCel}><Text>Salary</Text></View>
+        <View style = {styles.headCel}><Text>Age</Text></View>
+        <View style = {styles.headCel}><Text>Action</Text></View>
         </View>
         <FlatList
         style={styles.list}
@@ -30,10 +35,11 @@ export default function EmployeesList(Name){
         keyExtractor={item => ''+item.EmployeeID}
         renderItem={({item}) => (
             <View style = {styles.listItem}>
-            <View style = {styles.cel}><Text style = {styles.listText}>{item.Name}</Text></View>
-            <View style = {styles.cel}><Text style = {styles.listText}>{item.Position}</Text></View>
-            <View style = {styles.cel}><Text style = {styles.listText}>{item.Salary}</Text></View>
-            <View style = {styles.cel}><Text style = {styles.listText}>{item.age}</Text></View>
+            <View style = {styles.celName}><Text>{item.Name}</Text></View>
+            <View style = {styles. celPositionSalary}><Text>{item.Position}</Text></View>
+            <View style = {styles. celPositionSalary}><Text>{item.Salary}</Text></View>
+            <View style = {styles. celAge}><Text>{item.age}</Text></View>
+            <View style = {styles.celAction}><TouchableOpacity item={item.EmployeeID} onPress={deleteEmployee}><Text style = {styles.textDel}>Delete</Text></TouchableOpacity></View>
             </View>
         )}
        />
@@ -47,12 +53,40 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         borderWidth: 0.6
     },
-    cel:{
+    celName:{
         justifyContent: "center",
         alignItems: "center",
-        height: 40,
+        height: 50,
         width: width * 0.25 - 3.2,
         flexDirection: 'row',
+    },
+    textDel:{
+        color:'red'
+    },
+     celPositionSalary:{
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        width: width * 0.25 - 3.2,
+        flexDirection: 'row',
+        marginLeft: width * -0.01 - 11
+    },     
+      celAge:{
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        width: width * 0.22 - 3.2,
+        flexDirection: 'row',
+        marginLeft: width * -0.01 - 18,
+        marginRight: width * 0.13 - 18,
+    },
+    celAction:{
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        width: width * 0.19 - 3.2,
+        flexDirection: 'row',
+        marginLeft: width * -0.01 - 32
     },
     head:{
         backgroundColor: '#dcda48',
@@ -64,7 +98,18 @@ const styles = StyleSheet.create({
         borderStyle: 'solid',
         borderWidth:0.6,
         height: 40,
-        width: width * 0.25 - 3.2,
-        flexDirection: 'row'
+        width: width * 0.19 + 0.9,
+        flexDirection: 'row',
+        paddingLeft: 5
+    },
+    headCelPosition:{
+        justifyContent: "center",
+        alignItems: "center",
+        borderStyle: 'solid',
+        borderWidth:0.6,
+        height: 40,
+        width: width * 0.23 + 0.9,
+        flexDirection: 'row',
+        paddingLeft: 5
     },
   })
